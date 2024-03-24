ae29f508db92edc65fd5da51506d15aef4cf1de6
Dev-Costa <thiagocostadev03@gmail.com>
2024-03-23 21:25:14 -0300

    Changing the logic

--- main.py
+++ main.py
@@ -9,9 +9,10 @@ terms = letters + numbers
 password = ""
 for i in range(size):
 	random_index = randint(0, len(terms) - i)
-	while (terms[random_index] in password):
-		random_index = randint(0, len(terms) - i)
+	random_c = terms[random_index]
 
-	password += terms[random_index]
+	password += random_c
+
+	terms = terms.replace(random_c, "")
 
 print(password)
